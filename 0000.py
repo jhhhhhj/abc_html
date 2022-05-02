6f03dc0fd5189cf7fa20230a1cd83f444d846f01
jhhhhhj <104751668+jhhhhhj@users.noreply.github.com>
2022-05-02 15:17:18 +0900

    Add files via upload

---
+++ 0000.py
@@ -0,0 +1,34 @@
+import pygame
+import sys
+from time import sleep
+
+BLACK = (0,0,0)
+padWidth = 480
+padHeight = 640
+
+def initGame():
+    global gamePad, clock
+    pygame.init()
+    gamePad = pygame.display.set_mode((padWidth,padHeight))
+    pygame.display.set_caption('병뚜껑')
+    clock = pygame.time.Clock()
+    
+def runGame():
+    global gamepad, clock
+    
+    onGame = False
+    while not onGame:
+        for event in pygame.event.get():
+            if event.type in [pygame.QUIT]:
+                pygame.quit()
+                sys.exit()
+                
+            gamePad.fill(BLACK)
+            
+            pygame.display.update()
+            
+            clock.tick(60)
+        
+        pygame.quit()
+initGame()
+runGame()
